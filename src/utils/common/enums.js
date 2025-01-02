08111a10cde6814a19797658bb7026965f299d33
Kunal <kunalsabadra@gmail.com>
2025-01-02 22:47:47 +0530

    Added Seats Model associated to airplane

---
+++ src/utils/common/enums.js
@@ -0,0 +1,10 @@
+const SEAT_TYPE = {
+    BUSINESS: 'Business',
+    ECONOMY: 'economy',
+    PREMIUM_ECONOMY: 'premium-economy',
+    FIRST_CLASS: 'first-class'
+}
+
+module.exports = {
+    SEAT_TYPE
+}
